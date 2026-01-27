0097d47028995a9f940d40e11b6964a58220a8ec
Rickens Walter Otieno <rickensotieno@gmail.com>
2026-01-27 17:36:16 +0000

    Updates

---
+++ answers.sql
@@ -0,0 +1,19 @@
+SELECT *
+FROM public.green_trips
+WHERE lpep_pickup_datetime BETWEEN '2025-11-01' AND '2025-12-01';
+
+SELECT COUNT(*) AS short_trips
+FROM public.green_trips
+WHERE lpep_pickup_datetime >= '2025-11-01'
+  AND lpep_pickup_datetime < '2025-12-01'
+  AND trip_distance <= 1;
+
+
+SELECT 
+    DATE(lpep_pickup_datetime) AS pickup_day,
+    MAX(trip_distance) AS longest_trip_distance
+FROM public.green_trips
+WHERE trip_distance < 100
+GROUP BY pickup_day
+ORDER BY longest_trip_distance DESC
+LIMIT 1;
--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-def main():
-    print("Hello from datapipelines!")
+import pandas as pd
+from sqlalchemy import create_engine
 
+dates = ["lpep_pickup_datetime", "lpep_dropoff_datetime"]
+
+def run():
+    df = pd.read_parquet(
+        "https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2025-11.parquet",
+        engine="pyarrow"
+    )
+
+    for date in dates:
+        df[date] = pd.to_datetime(df[date])
+
+    engine = create_engine("postgresql://root:root@localhost:5432/ny_taxi")
+
+    df.head(0).to_sql("green_trips", engine, if_exists="replace", index=False)
+    df.to_sql("green_trips", engine, if_exists="append", index=False, chunksize=100_000)
+
+    zones = pd.read_csv(
+        "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/misc/taxi_zone_lookup.csv"
+    )
+    zones.head(0).to_sql("zones", engine, if_exists="replace", index=False)
+    zones.to_sql("zones", engine, if_exists="append", index=False)
 
 if __name__ == "__main__":
-    main()
+    run()
